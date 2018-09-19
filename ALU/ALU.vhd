78d59eedb9f82596403b5ecbb39948102fa3ef85
Joaquín Domínguez Moran <joaks.jdm@gmail.com>
2018-09-19 12:04:18 -0500

    Update ALU.vhd

--- ALU/ALU.vhd
+++ ALU/ALU.vhd
@@ -1,37 +1,15 @@
 --Create Date:    19:28:01 09/12/2018 
--- Design Name: 
 -- Module Name:    ALU - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity ALU is
 	GENERIC ( N: INTEGER:=4);
     Port ( A : in  STD_LOGIC_VECTOR (N-1 downto 0);
            B : in  STD_LOGIC_VECTOR (N-1 downto 0);
-			  ALUOP: in  STD_LOGIC_VECTOR (3 downto 0);
+	   ALUOP: in  STD_LOGIC_VECTOR (3 downto 0);
            RESULT : out  STD_LOGIC_VECTOR (N-1 downto 0);
-			  FLAG: out STD_LOGIC_VECTOR (3 downto 0)
+	   FLAG: out STD_LOGIC_VECTOR (3 downto 0)
            );
 end ALU;
 --ALUOP(3)- AINVERT
@@ -75,7 +53,7 @@ begin
 					FOR K IN J+1 TO I LOOP
 					Temporal:= (Temporal AND P(K)); 
 					END LOOP;
-			   Temporal1:= (Temporal1 OR (G(J) AND Temporal));
+			   	Temporal1:= (Temporal1 OR (G(J) AND Temporal));
 				END LOOP;
 				Temporal:='1';
 				Temporal2:='0';
@@ -85,15 +63,13 @@ begin
 				Temporal2:= (Temporal2 OR (C(0) AND Temporal));
 				C(I+1):= (G(I) OR Temporal1 OR Temporal2 );
 				FLAG(0) <= C(N);
-				FLAG(3) <= C(N) XOR C(N-1);
-				
+				FLAG(3) <= C(N) XOR C(N-1);	
 		END CASE;
 		RESULT <=RES;
 		FLAG(2) <= RES(3);
 		FLAG(1) <= NOT (RES(0) OR RES(1) OR RES(2) OR RES(3) );
 		END LOOP;
 		END PROCESS;
-		
 end Behavioral;
 
 
